3b4ccef92ec069bf1748523d0b03bbc89f726933
Kaique Geraldo <kaiique2404@gmail.com>
2026-06-08 01:32:45 -0300

    ajuste dataSeeder

--- hbu-backend/src/main/java/com/hbu/unimar/infra/seed/DataSeeder.java
+++ hbu-backend/src/main/java/com/hbu/unimar/infra/seed/DataSeeder.java
@@ -1,6 +1,7 @@
 package com.hbu.unimar.infra.seed;
 
 import com.hbu.unimar.domain.dto.*;
+import com.hbu.unimar.domain.enums.*;
 import com.hbu.unimar.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Slf4j
@@ -41,70 +43,112 @@ public class DataSeeder implements CommandLineRunner {
         );
 
         // 2. Criar Salas (6 salas)
-        var s1 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 01", "SALA_PADRAO", "DISPONIVEL"));
-        var s2 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 02", "SALA_PADRAO", "DISPONIVEL"));
-        var s3 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 03", "SALA_ALTA_COMPLEXIDADE", "DISPONIVEL"));
-        var s4 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 04", "SALA_ALTA_COMPLEXIDADE", "DISPONIVEL"));
-        var s5 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 05 - Parto", "SALA_PARTO", "DISPONIVEL"));
-        var s6 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 06 - Trauma", "SALA_EMERGENCIA", "DISPONIVEL"));
+        var s1 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 01", TipoSala.SALA_PADRAO, "DISPONIVEL"));
+        var s2 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 02", TipoSala.SALA_PADRAO, "DISPONIVEL"));
+        var s3 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 03", TipoSala.SALA_ALTA_COMPLEXIDADE, "DISPONIVEL"));
+        var s4 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 04", TipoSala.SALA_ALTA_COMPLEXIDADE, "DISPONIVEL"));
+        var s5 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 05 - Parto", TipoSala.SALA_PARTO, "DISPONIVEL"));
+        var s6 = salaService.criarSala(new SalaCirurgicaRequestDTO("Sala 06 - Trauma", TipoSala.SALA_EMERGENCIA, "DISPONIVEL"));
 
         // 3. Criar Profissionais (10 profissionais)
-        var p1 = profissionalService.criarProfissional(new ProfissionalRequestDTO("Dr. Kaique Santos", "CRM 11111", "CIRURGIA_GERAL"));
-        var p2 = profissionalService.criarProfissional(new ProfissionalRequestDTO("Dr. Kauã Silva", "CRM 22222", "ANESTESIOLOGIA"));
-        var p3 = profissionalService.criarProfissional(new ProfissionalRequestDTO("Dra. Gabi Silva", "CRM 33333", "ORTOPEDIA"));
-        var p4 = profissionalService.criarProfissional(new ProfissionalRequestDTO("Dr. Mateus Oliveira", "CRM 44444", "CIRURGIA_GERAL"));
-        var p5 = profissionalService.criarProfissional(new ProfissionalRequestDTO("Dra. Fernanda Lima", "CRM 55555", "ANESTESIOLOGIA"));
-        profissionalService.criarProfissional(new ProfissionalRequestDTO("Enf. Carlos Heinrich", "COREN 66666", "ENFERMAGEM"));
-        profissionalService.criarProfissional(new ProfissionalRequestDTO("Enf. João Vitor", "COREN 77777", "ENFERMAGEM"));
-        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dr. Luiz Antônio", "CRM 88888", "ORTOPEDIA"));
-        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dr. Marcos Tulio", "CRM 99999", "CIRURGIA_GERAL"));
-        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dra. Amanda Rocha", "CRM 10101", "ANESTESIOLOGIA"));
+        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dr. Kaique Santos",  "CRM 11111", "Cirurgia Geral"));
+        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dr. Kauã Silva",     "CRM 22222", "Anestesiologia"));
+        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dra. Gabi Silva",    "CRM 33333", "Instrumentação Cirúrgica"));
+        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dr. Mateus Oliveira","CRM 44444", "Cirurgia Geral"));
+        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dra. Fernanda Lima", "CRM 55555", "Anestesiologia"));
+        profissionalService.criarProfissional(new ProfissionalRequestDTO("Enf. Carlos Heinrich","COREN 66666", "Enfermagem Cirúrgica"));
+        profissionalService.criarProfissional(new ProfissionalRequestDTO("Enf. João Vitor",    "COREN 77777", "Enfermagem Cirúrgica"));
+        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dr. Luiz Antônio",   "CRM 88888", "Instrumentação Cirúrgica"));
+        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dr. Marcos Tulio",   "CRM 99999", "Cirurgia Geral"));
+        profissionalService.criarProfissional(new ProfissionalRequestDTO("Dra. Amanda Rocha",  "CRM 10101", "Anestesiologia"));
 
         // 4. Criar Pacientes (12 pacientes)
-        var pac1 = pacienteService.criarPaciente(new PacienteRequestDTO("João da Silva", "11122233344", "1980-05-15"));
-        var pac2 = pacienteService.criarPaciente(new PacienteRequestDTO("Maria Oliveira", "22233344455", "1992-08-20"));
-        var pac3 = pacienteService.criarPaciente(new PacienteRequestDTO("Carlos Pereira", "33344455566", "1975-11-10"));
-        var pac4 = pacienteService.criarPaciente(new PacienteRequestDTO("Ana Costa", "44455566677", "2000-02-25"));
-        var pac5 = pacienteService.criarPaciente(new PacienteRequestDTO("Pedro Santos", "55566677788", "1988-06-30"));
-        var pac6 = pacienteService.criarPaciente(new PacienteRequestDTO("Lucas Ferreira", "66677788899", "1995-09-05"));
-        var pac7 = pacienteService.criarPaciente(new PacienteRequestDTO("Mariana Lima", "77788899900", "1982-12-12"));
-        var pac8 = pacienteService.criarPaciente(new PacienteRequestDTO("Fernando Gomes", "88899900011", "1970-04-18"));
-        var pac9 = pacienteService.criarPaciente(new PacienteRequestDTO("Beatriz Martins", "99900011122", "1998-07-22"));
-        var pac10 = pacienteService.criarPaciente(new PacienteRequestDTO("Rafael Carvalho", "12121212121", "1985-10-08"));
-        var pac11 = pacienteService.criarPaciente(new PacienteRequestDTO("Juliana Ribeiro", "23232323232", "1990-01-14"));
-        var pac12 = pacienteService.criarPaciente(new PacienteRequestDTO("Diego Araújo", "34343434343", "1978-03-27"));
+        var pac1  = pacienteService.criarPaciente(new PacienteRequestDTO("João da Silva",    "111.222.333-44", LocalDate.of(1980,  5, 15)));
+        var pac2  = pacienteService.criarPaciente(new PacienteRequestDTO("Maria Oliveira",   "222.333.444-55", LocalDate.of(1992,  8, 20)));
+        var pac3  = pacienteService.criarPaciente(new PacienteRequestDTO("Carlos Pereira",   "333.444.555-66", LocalDate.of(1975, 11, 10)));
+        var pac4  = pacienteService.criarPaciente(new PacienteRequestDTO("Ana Costa",        "444.555.666-77", LocalDate.of(2000,  2, 25)));
+        var pac5  = pacienteService.criarPaciente(new PacienteRequestDTO("Pedro Santos",     "555.666.777-88", LocalDate.of(1988,  6, 30)));
+        var pac6  = pacienteService.criarPaciente(new PacienteRequestDTO("Lucas Ferreira",   "666.777.888-99", LocalDate.of(1995,  9,  5)));
+        var pac7  = pacienteService.criarPaciente(new PacienteRequestDTO("Mariana Lima",     "777.888.999-00", LocalDate.of(1982, 12, 12)));
+        var pac8  = pacienteService.criarPaciente(new PacienteRequestDTO("Fernando Gomes",   "888.999.000-11", LocalDate.of(1970,  4, 18)));
+        var pac9  = pacienteService.criarPaciente(new PacienteRequestDTO("Beatriz Martins",  "999.000.111-22", LocalDate.of(1998,  7, 22)));
+        var pac10 = pacienteService.criarPaciente(new PacienteRequestDTO("Rafael Carvalho",  "121.212.121-21", LocalDate.of(1985, 10,  8)));
+        var pac11 = pacienteService.criarPaciente(new PacienteRequestDTO("Juliana Ribeiro",  "232.323.232-32", LocalDate.of(1990,  1, 14)));
+        var pac12 = pacienteService.criarPaciente(new PacienteRequestDTO("Diego Araújo",     "343.434.343-43", LocalDate.of(1978,  3, 27)));
 
         // 5. Criar Procedimentos TUSS (8 procedimentos)
-        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("31005470", "Apendicectomia", 90, "SALA_PADRAO"));
-        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("31009220", "Colecistectomia Videolaparoscópica", 120, "SALA_ALTA_COMPLEXIDADE"));
-        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("30715018", "Artroplastia Total do Joelho", 180, "SALA_ALTA_COMPLEXIDADE"));
-        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("31309054", "Cesariana", 60, "SALA_PARTO"));
-        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("30205095", "Traqueostomia", 45, "SALA_EMERGENCIA"));
-        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("30906107", "Revascularização Miocárdica", 240, "SALA_ALTA_COMPLEXIDADE"));
-        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("31102026", "Herniorrafia Inguinal", 60, "SALA_PADRAO"));
-        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("30602264", "Amigdalectomia", 45, "SALA_PADRAO"));
+        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("31005470", "Apendicectomia",                    90,  TipoSala.SALA_PADRAO));
+        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("31009220", "Colecistectomia Videolaparoscópica", 120, TipoSala.SALA_ALTA_COMPLEXIDADE));
+        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("30715018", "Artroplastia Total do Joelho",       180, TipoSala.SALA_ALTA_COMPLEXIDADE));
+        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("31309054", "Cesariana",                          60, TipoSala.SALA_PARTO));
+        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("30205095", "Traqueostomia",                      45, TipoSala.SALA_EMERGENCIA));
+        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("30906107", "Revascularização Miocárdica",        240, TipoSala.SALA_ALTA_COMPLEXIDADE));
+        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("31102026", "Herniorrafia Inguinal",               60, TipoSala.SALA_PADRAO));
+        procedimentoService.criarProcedimento(new ProcedimentoCirurgicoRequestDTO("30602264", "Amigdalectomia",                     45, TipoSala.SALA_PADRAO));
 
         // 6. Agendar Cirurgias
         // DIA 11
-        String dia11 = "2026-06-11";
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac1.id().toString(), s1.id().toString(), "ELETIVA", dia11 + "T08:00:00-03:00", dia11 + "T10:00:00-03:00"));
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac2.id().toString(), s2.id().toString(), "URGENCIA", dia11 + "T10:30:00-03:00", dia11 + "T12:30:00-03:00"));
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac1.id(), s1.id(), Prioridade.ELETIVA,
+                ZonedDateTime.parse("2026-06-11T08:00:00-03:00"),
+                ZonedDateTime.parse("2026-06-11T10:00:00-03:00")));
+
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac2.id(), s2.id(), Prioridade.URGENCIA,
+                ZonedDateTime.parse("2026-06-11T10:30:00-03:00"),
+                ZonedDateTime.parse("2026-06-11T12:30:00-03:00")));
 
         // DIA 12
-        String dia12 = "2026-06-12";
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac3.id().toString(), s1.id().toString(), "ELETIVA", dia12 + "T07:00:00-03:00", dia12 + "T09:00:00-03:00"));
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac4.id().toString(), s2.id().toString(), "ELETIVA", dia12 + "T08:30:00-03:00", dia12 + "T11:00:00-03:00"));
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac5.id().toString(), s3.id().toString(), "URGENCIA", dia12 + "T10:00:00-03:00", dia12 + "T14:00:00-03:00"));
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac6.id().toString(), s4.id().toString(), "ELETIVA", dia12 + "T13:00:00-03:00", dia12 + "T15:30:00-03:00"));
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac7.id().toString(), s5.id().toString(), "ELETIVA", dia12 + "T14:00:00-03:00", dia12 + "T16:00:00-03:00"));
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac8.id().toString(), s6.id().toString(), "EMERGENCIA", dia12 + "T16:30:00-03:00", dia12 + "T18:00:00-03:00"));
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac3.id(), s1.id(), Prioridade.ELETIVA,
+                ZonedDateTime.parse("2026-06-12T07:00:00-03:00"),
+                ZonedDateTime.parse("2026-06-12T09:00:00-03:00")));
+
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac4.id(), s2.id(), Prioridade.ELETIVA,
+                ZonedDateTime.parse("2026-06-12T08:30:00-03:00"),
+                ZonedDateTime.parse("2026-06-12T11:00:00-03:00")));
+
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac5.id(), s3.id(), Prioridade.URGENCIA,
+                ZonedDateTime.parse("2026-06-12T10:00:00-03:00"),
+                ZonedDateTime.parse("2026-06-12T14:00:00-03:00")));
+
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac6.id(), s4.id(), Prioridade.ELETIVA,
+                ZonedDateTime.parse("2026-06-12T13:00:00-03:00"),
+                ZonedDateTime.parse("2026-06-12T15:30:00-03:00")));
+
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac7.id(), s5.id(), Prioridade.ELETIVA,
+                ZonedDateTime.parse("2026-06-12T14:00:00-03:00"),
+                ZonedDateTime.parse("2026-06-12T16:00:00-03:00")));
+
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac8.id(), s6.id(), Prioridade.EMERGENCIA,
+                ZonedDateTime.parse("2026-06-12T16:30:00-03:00"),
+                ZonedDateTime.parse("2026-06-12T18:00:00-03:00")));
 
         // DIA 13
-        String dia13 = "2026-06-13";
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac9.id().toString(), s1.id().toString(), "ELETIVA", dia13 + "T08:00:00-03:00", dia13 + "T09:30:00-03:00"));
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac10.id().toString(), s2.id().toString(), "ELETIVA", dia13 + "T09:00:00-03:00", dia13 + "T11:00:00-03:00"));
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac11.id().toString(), s3.id().toString(), "URGENCIA", dia13 + "T13:00:00-03:00", dia13 + "T15:00:00-03:00"));
-        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(pac12.id().toString(), s4.id().toString(), "ELETIVA", dia13 + "T15:30:00-03:00", dia13 + "T17:30:00-03:00"));
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac9.id(), s1.id(), Prioridade.ELETIVA,
+                ZonedDateTime.parse("2026-06-13T08:00:00-03:00"),
+                ZonedDateTime.parse("2026-06-13T09:30:00-03:00")));
+
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac10.id(), s2.id(), Prioridade.ELETIVA,
+                ZonedDateTime.parse("2026-06-13T09:00:00-03:00"),
+                ZonedDateTime.parse("2026-06-13T11:00:00-03:00")));
+
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac11.id(), s3.id(), Prioridade.URGENCIA,
+                ZonedDateTime.parse("2026-06-13T13:00:00-03:00"),
+                ZonedDateTime.parse("2026-06-13T15:00:00-03:00")));
+
+        cirurgiaService.agendarCirurgia(new AgendamentoCirurgiaDTO(
+                pac12.id(), s4.id(), Prioridade.ELETIVA,
+                ZonedDateTime.parse("2026-06-13T15:30:00-03:00"),
+                ZonedDateTime.parse("2026-06-13T17:30:00-03:00")));
 
         log.info("Massa de dados injetada com sucesso!");
         SecurityContextHolder.clearContext();
