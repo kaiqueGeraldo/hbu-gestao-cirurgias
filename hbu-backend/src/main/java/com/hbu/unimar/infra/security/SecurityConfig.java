d41d77561f99d5e2b6ab8b24cd607f210af31371
Kaique Geraldo <kaiique2404@gmail.com>
2026-06-08 02:28:27 -0300

    implementa url frontend

--- hbu-backend/src/main/java/com/hbu/unimar/infra/security/SecurityConfig.java
+++ hbu-backend/src/main/java/com/hbu/unimar/infra/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://hbu-gestao-cirurgias.vercel.app/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://hbu-gestao-cirurgias.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
